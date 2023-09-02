180ed4e8e137dae8c1b2cb61eda00f18ce2df270
TrimokRu <prokirik@gmail.com>
2023-09-02 13:07:36 +0300

    Update DragAndDrop.kt

--- drag-and-drop-compose/src/main/java/com/jgomezdev/draganddropcompose/DragAndDrop.kt
+++ drag-and-drop-compose/src/main/java/com/jgomezdev/draganddropcompose/DragAndDrop.kt
@@ -51,120 +51,123 @@ import kotlin.math.sign
 class DragAndDrop(){
     val status = MutableLiveData<ApsState>(ApsState.Loading)
 
-    fun <T> Modifier.dragAndDrop(
-        item: T,
-        items: MutableList<T>,
-        itemHeight: Int,
-        updateSlideState: (item: T, slideState: SlideState) -> Unit,
-        isDraggedAfterLongPress: Boolean,
-        onStartDrag: () -> Unit,
-        onStopDrag: (currentIndex: Int, destinationIndex: Int) -> Unit,
-    ): Modifier = composed {
-        val offsetX = remember { Animatable(0f) }
-        val offsetY = remember { Animatable(0f) }
-        pointerInput(Unit) {
-            // Wrap in a coroutine scope to use suspend functions for touch events and animation.
-            coroutineScope {
-                val shoesArticleIndex = items.indexOf(item)
-                val offsetToSlide = itemHeight / 4
-                var numberOfItems = 0
-                var previousNumberOfItems: Int
-                var listOffset = 0
+    companion object{
+        fun <T> Modifier.dragAndDrop(
+            item: T,
+            items: MutableList<T>,
+            itemHeight: Int,
+            updateSlideState: (item: T, slideState: SlideState) -> Unit,
+            isDraggedAfterLongPress: Boolean,
+            onStartDrag: () -> Unit,
+            onStopDrag: (currentIndex: Int, destinationIndex: Int) -> Unit,
+        ): Modifier = composed {
+            val offsetX = remember { Animatable(0f) }
+            val offsetY = remember { Animatable(0f) }
+            pointerInput(Unit) {
+                // Wrap in a coroutine scope to use suspend functions for touch events and animation.
+                coroutineScope {
+                    val shoesArticleIndex = items.indexOf(item)
+                    val offsetToSlide = itemHeight / 4
+                    var numberOfItems = 0
+                    var previousNumberOfItems: Int
+                    var listOffset = 0
 
-                val onDragStart = {
-                    // Interrupt any ongoing animation of other items.
-                    launch {
-                        offsetX.stop()
-                        offsetY.stop()
+                    val onDragStart = {
+                        // Interrupt any ongoing animation of other items.
+                        launch {
+                            offsetX.stop()
+                            offsetY.stop()
+                        }
+                        onStartDrag()
                     }
-                    onStartDrag()
-                }
-                val onDrag = {change: PointerInputChange ->
-                    val horizontalDragOffset = offsetX.value + change.positionChange().x
-                    launch {
-                        offsetX.snapTo(horizontalDragOffset)
-                    }
-                    val verticalDragOffset = offsetY.value + change.positionChange().y
-                    launch {
-                        offsetY.snapTo(verticalDragOffset)
-                        val offsetSign = offsetY.value.sign.toInt()
-                        previousNumberOfItems = numberOfItems
-                        numberOfItems = calculateNumberOfSlidItems(
-                            offsetY.value * offsetSign,
-                            itemHeight,
-                            offsetToSlide,
-                            previousNumberOfItems
-                        )
-
-                        if (previousNumberOfItems > numberOfItems) {
-                            updateSlideState(
-                                items[shoesArticleIndex + previousNumberOfItems * offsetSign],
-                                SlideState.NONE
+                    val onDrag = {change: PointerInputChange ->
+                        val horizontalDragOffset = offsetX.value + change.positionChange().x
+                        launch {
+                            offsetX.snapTo(horizontalDragOffset)
+                        }
+                        val verticalDragOffset = offsetY.value + change.positionChange().y
+                        launch {
+                            offsetY.snapTo(verticalDragOffset)
+                            val offsetSign = offsetY.value.sign.toInt()
+                            previousNumberOfItems = numberOfItems
+                            numberOfItems = calculateNumberOfSlidItems(
+                                offsetY.value * offsetSign,
+                                itemHeight,
+                                offsetToSlide,
+                                previousNumberOfItems
                             )
-                        } else if (numberOfItems != 0) {
-                            try {
-                                updateSlideState(
-                                    items[shoesArticleIndex + numberOfItems * offsetSign],
-                                    if (offsetSign == 1) SlideState.UP else SlideState.DOWN
-                                )
-                            } catch (e: IndexOutOfBoundsException) {
-                                numberOfItems = previousNumberOfItems
-                                Log.i("DragToReorder", "Item is outside or at the edge")
-                            }
-                        }
-                        listOffset = numberOfItems * offsetSign
-                    }
-                    // Consume the gesture event, not passed to external
-                    change.consumePositionChange()
-                }
-                val onDragEnd = {
-                    launch {
-                        offsetX.animateTo(0f)
-                    }
-                    launch {
-                        offsetY.animateTo(itemHeight * numberOfItems * offsetY.value.sign)
-                        onStopDrag(shoesArticleIndex, shoesArticleIndex + listOffset)
-                    }
-                }
-                if (isDraggedAfterLongPress)
-                    detectDragGesturesAfterLongPress(
-                        onDragStart = { onDragStart() },
-                        onDrag = { change, _ -> onDrag(change) },
-                        onDragEnd = { onDragEnd() }
-                    ) else
-                    while (true) {
-                        val pointerId = awaitPointerEventScope { awaitFirstDown().id }
-                        awaitPointerEventScope {
-                            drag(pointerId) { change ->
-                                onDragStart()
-                                onDrag(change)
-                            }
-                        }
-                        onDragEnd()
-                    }
-            }
-        }
-            .offset {
-                // Use the animating offset value here.
-                IntOffset(offsetX.value.roundToInt(), offsetY.value.roundToInt())
-            }
-    }
 
-    private fun calculateNumberOfSlidItems(
-        offsetY: Float,
-        itemHeight: Int,
-        offsetToSlide: Int,
-        previousNumberOfItems: Int
-    ): Int {
-        val numberOfItemsInOffset = (offsetY / itemHeight).toInt()
-        val numberOfItemsPlusOffset = ((offsetY + offsetToSlide) / itemHeight).toInt()
-        val numberOfItemsMinusOffset = ((offsetY - offsetToSlide - 1) / itemHeight).toInt()
-        return when {
-            offsetY - offsetToSlide - 1 < 0 -> 0
-            numberOfItemsPlusOffset > numberOfItemsInOffset -> numberOfItemsPlusOffset
-            numberOfItemsMinusOffset < numberOfItemsInOffset -> numberOfItemsInOffset
-            else -> previousNumberOfItems
+                            if (previousNumberOfItems > numberOfItems) {
+                                updateSlideState(
+                                    items[shoesArticleIndex + previousNumberOfItems * offsetSign],
+                                    SlideState.NONE
+                                )
+                            } else if (numberOfItems != 0) {
+                                try {
+                                    updateSlideState(
+                                        items[shoesArticleIndex + numberOfItems * offsetSign],
+                                        if (offsetSign == 1) SlideState.UP else SlideState.DOWN
+                                    )
+                                } catch (e: IndexOutOfBoundsException) {
+                                    numberOfItems = previousNumberOfItems
+                                    Log.i("DragToReorder", "Item is outside or at the edge")
+                                }
+                            }
+                            listOffset = numberOfItems * offsetSign
+                        }
+                        // Consume the gesture event, not passed to external
+                        change.consumePositionChange()
+                    }
+                    val onDragEnd = {
+                        launch {
+                            offsetX.animateTo(0f)
+                        }
+                        launch {
+                            offsetY.animateTo(itemHeight * numberOfItems * offsetY.value.sign)
+                            onStopDrag(shoesArticleIndex, shoesArticleIndex + listOffset)
+                        }
+                    }
+                    if (isDraggedAfterLongPress)
+                        detectDragGesturesAfterLongPress(
+                            onDragStart = { onDragStart() },
+                            onDrag = { change, _ -> onDrag(change) },
+                            onDragEnd = { onDragEnd() }
+                        ) else
+                        while (true) {
+                            val pointerId = awaitPointerEventScope { awaitFirstDown().id }
+                            awaitPointerEventScope {
+                                drag(pointerId) { change ->
+                                    onDragStart()
+                                    onDrag(change)
+                                }
+                            }
+                            onDragEnd()
+                        }
+                }
+            }
+                .offset {
+                    // Use the animating offset value here.
+                    IntOffset(offsetX.value.roundToInt(), offsetY.value.roundToInt())
+                }
         }
+
+        private fun calculateNumberOfSlidItems(
+            offsetY: Float,
+            itemHeight: Int,
+            offsetToSlide: Int,
+            previousNumberOfItems: Int
+        ): Int {
+            val numberOfItemsInOffset = (offsetY / itemHeight).toInt()
+            val numberOfItemsPlusOffset = ((offsetY + offsetToSlide) / itemHeight).toInt()
+            val numberOfItemsMinusOffset = ((offsetY - offsetToSlide - 1) / itemHeight).toInt()
+            return when {
+                offsetY - offsetToSlide - 1 < 0 -> 0
+                numberOfItemsPlusOffset > numberOfItemsInOffset -> numberOfItemsPlusOffset
+                numberOfItemsMinusOffset < numberOfItemsInOffset -> numberOfItemsInOffset
+                else -> previousNumberOfItems
+            }
+        }
+
     }
 
 
