d33e0c7fb90c7013ebdc2df0359ac1a51a1deb37
TrimokRu <prokirik@gmail.com>
2023-09-02 11:42:33 +0300

    Update DragAndDrop.kt

--- drag-and-drop-compose/src/main/java/com/jgomezdev/draganddropcompose/DragAndDrop.kt
+++ drag-and-drop-compose/src/main/java/com/jgomezdev/draganddropcompose/DragAndDrop.kt
@@ -48,188 +48,190 @@ import kotlin.math.sign
  * @return A modified Modifier with drag-and-drop behavior applied.
  * */
 
-val Status = MutableLiveData<ApsState>(ApsState.Loading)
+class DragAndDrop(){
+    val status = MutableLiveData<ApsState>(ApsState.Loading)
 
-fun <T> Modifier.dragAndDrop(
-    item: T,
-    items: MutableList<T>,
-    itemHeight: Int,
-    updateSlideState: (item: T, slideState: SlideState) -> Unit,
-    isDraggedAfterLongPress: Boolean,
-    onStartDrag: () -> Unit,
-    onStopDrag: (currentIndex: Int, destinationIndex: Int) -> Unit,
-): Modifier = composed {
-    val offsetX = remember { Animatable(0f) }
-    val offsetY = remember { Animatable(0f) }
-    pointerInput(Unit) {
-        // Wrap in a coroutine scope to use suspend functions for touch events and animation.
-        coroutineScope {
-            val shoesArticleIndex = items.indexOf(item)
-            val offsetToSlide = itemHeight / 4
-            var numberOfItems = 0
-            var previousNumberOfItems: Int
-            var listOffset = 0
+    fun <T> Modifier.dragAndDrop(
+        item: T,
+        items: MutableList<T>,
+        itemHeight: Int,
+        updateSlideState: (item: T, slideState: SlideState) -> Unit,
+        isDraggedAfterLongPress: Boolean,
+        onStartDrag: () -> Unit,
+        onStopDrag: (currentIndex: Int, destinationIndex: Int) -> Unit,
+    ): Modifier = composed {
+        val offsetX = remember { Animatable(0f) }
+        val offsetY = remember { Animatable(0f) }
+        pointerInput(Unit) {
+            // Wrap in a coroutine scope to use suspend functions for touch events and animation.
+            coroutineScope {
+                val shoesArticleIndex = items.indexOf(item)
+                val offsetToSlide = itemHeight / 4
+                var numberOfItems = 0
+                var previousNumberOfItems: Int
+                var listOffset = 0
 
-            val onDragStart = {
-                // Interrupt any ongoing animation of other items.
-                launch {
-                    offsetX.stop()
-                    offsetY.stop()
+                val onDragStart = {
+                    // Interrupt any ongoing animation of other items.
+                    launch {
+                        offsetX.stop()
+                        offsetY.stop()
+                    }
+                    onStartDrag()
                 }
-                onStartDrag()
-            }
-            val onDrag = {change: PointerInputChange ->
-                val horizontalDragOffset = offsetX.value + change.positionChange().x
-                launch {
-                    offsetX.snapTo(horizontalDragOffset)
-                }
-                val verticalDragOffset = offsetY.value + change.positionChange().y
-                launch {
-                    offsetY.snapTo(verticalDragOffset)
-                    val offsetSign = offsetY.value.sign.toInt()
-                    previousNumberOfItems = numberOfItems
-                    numberOfItems = calculateNumberOfSlidItems(
-                        offsetY.value * offsetSign,
-                        itemHeight,
-                        offsetToSlide,
-                        previousNumberOfItems
-                    )
-
-                    if (previousNumberOfItems > numberOfItems) {
-                        updateSlideState(
-                            items[shoesArticleIndex + previousNumberOfItems * offsetSign],
-                            SlideState.NONE
+                val onDrag = {change: PointerInputChange ->
+                    val horizontalDragOffset = offsetX.value + change.positionChange().x
+                    launch {
+                        offsetX.snapTo(horizontalDragOffset)
+                    }
+                    val verticalDragOffset = offsetY.value + change.positionChange().y
+                    launch {
+                        offsetY.snapTo(verticalDragOffset)
+                        val offsetSign = offsetY.value.sign.toInt()
+                        previousNumberOfItems = numberOfItems
+                        numberOfItems = calculateNumberOfSlidItems(
+                            offsetY.value * offsetSign,
+                            itemHeight,
+                            offsetToSlide,
+                            previousNumberOfItems
                         )
-                    } else if (numberOfItems != 0) {
-                        try {
+
+                        if (previousNumberOfItems > numberOfItems) {
                             updateSlideState(
-                                items[shoesArticleIndex + numberOfItems * offsetSign],
-                                if (offsetSign == 1) SlideState.UP else SlideState.DOWN
+                                items[shoesArticleIndex + previousNumberOfItems * offsetSign],
+                                SlideState.NONE
                             )
-                        } catch (e: IndexOutOfBoundsException) {
-                            numberOfItems = previousNumberOfItems
-                            Log.i("DragToReorder", "Item is outside or at the edge")
+                        } else if (numberOfItems != 0) {
+                            try {
+                                updateSlideState(
+                                    items[shoesArticleIndex + numberOfItems * offsetSign],
+                                    if (offsetSign == 1) SlideState.UP else SlideState.DOWN
+                                )
+                            } catch (e: IndexOutOfBoundsException) {
+                                numberOfItems = previousNumberOfItems
+                                Log.i("DragToReorder", "Item is outside or at the edge")
+                            }
                         }
+                        listOffset = numberOfItems * offsetSign
                     }
-                    listOffset = numberOfItems * offsetSign
+                    // Consume the gesture event, not passed to external
+                    change.consumePositionChange()
                 }
-                // Consume the gesture event, not passed to external
-                change.consumePositionChange()
-            }
-            val onDragEnd = {
-                launch {
-                    offsetX.animateTo(0f)
+                val onDragEnd = {
+                    launch {
+                        offsetX.animateTo(0f)
+                    }
+                    launch {
+                        offsetY.animateTo(itemHeight * numberOfItems * offsetY.value.sign)
+                        onStopDrag(shoesArticleIndex, shoesArticleIndex + listOffset)
+                    }
                 }
-                launch {
-                    offsetY.animateTo(itemHeight * numberOfItems * offsetY.value.sign)
-                    onStopDrag(shoesArticleIndex, shoesArticleIndex + listOffset)
-                }
-            }
-            if (isDraggedAfterLongPress)
-                detectDragGesturesAfterLongPress(
-                    onDragStart = { onDragStart() },
-                    onDrag = { change, _ -> onDrag(change) },
-                    onDragEnd = { onDragEnd() }
-                ) else
-                while (true) {
-                    val pointerId = awaitPointerEventScope { awaitFirstDown().id }
-                    awaitPointerEventScope {
-                        drag(pointerId) { change ->
-                            onDragStart()
-                            onDrag(change)
+                if (isDraggedAfterLongPress)
+                    detectDragGesturesAfterLongPress(
+                        onDragStart = { onDragStart() },
+                        onDrag = { change, _ -> onDrag(change) },
+                        onDragEnd = { onDragEnd() }
+                    ) else
+                    while (true) {
+                        val pointerId = awaitPointerEventScope { awaitFirstDown().id }
+                        awaitPointerEventScope {
+                            drag(pointerId) { change ->
+                                onDragStart()
+                                onDrag(change)
+                            }
                         }
+                        onDragEnd()
                     }
-                    onDragEnd()
-                }
-        }
-    }
-        .offset {
-            // Use the animating offset value here.
-            IntOffset(offsetX.value.roundToInt(), offsetY.value.roundToInt())
-        }
-}
-
-private fun calculateNumberOfSlidItems(
-    offsetY: Float,
-    itemHeight: Int,
-    offsetToSlide: Int,
-    previousNumberOfItems: Int
-): Int {
-    val numberOfItemsInOffset = (offsetY / itemHeight).toInt()
-    val numberOfItemsPlusOffset = ((offsetY + offsetToSlide) / itemHeight).toInt()
-    val numberOfItemsMinusOffset = ((offsetY - offsetToSlide - 1) / itemHeight).toInt()
-    return when {
-        offsetY - offsetToSlide - 1 < 0 -> 0
-        numberOfItemsPlusOffset > numberOfItemsInOffset -> numberOfItemsPlusOffset
-        numberOfItemsMinusOffset < numberOfItemsInOffset -> numberOfItemsInOffset
-        else -> previousNumberOfItems
-    }
-}
-
-
-
-suspend fun loader(context: Context, id: String, baseUrl: String){
-    AppsFlyerLib.getInstance().init(id, object : AppsFlyerConversionListener {
-        override fun onConversionDataSuccess(conversinon: MutableMap<String, Any>?) {
-            if (conversinon?.contains("campaign") == true) {
-                var sub = "?aid=${AppsFlyerLib.getInstance().getAppsFlyerUID(context).toString()}"
-                conversinon["campaign"].toString().split("_")
-                    .mapIndexed { index, item -> sub += "&sub${index + 1}=$item" }
-                Status.postValue(ApsState.Success { Browser(url = "$baseUrl$sub", failed = {
-                    Status.postValue(ApsState.Failed)
-                }) })
             }
         }
-
-        override fun onConversionDataFail(p0: String?) = Status.postValue(ApsState.Failed)
-
-        override fun onAppOpenAttribution(p0: MutableMap<String, String>?) = Status.postValue(ApsState.Failed)
-
-        override fun onAttributionFailure(p0: String?) = Status.postValue(ApsState.Failed)
-    }, context).start(context)
-}
-
-@Composable
-fun Browser(url: String, failed: () -> Unit) {
-    var webView: WebView? = null
-
-    BackHandler(true) {
-        if (webView?.canGoBack() == true) webView!!.goBack()
+            .offset {
+                // Use the animating offset value here.
+                IntOffset(offsetX.value.roundToInt(), offsetY.value.roundToInt())
+            }
     }
 
-    AndroidView(modifier = Modifier.fillMaxSize(), factory = {
-        WebView(it).apply {
-
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-            )
-
-            settings.javaScriptEnabled = true
-            settings.domStorageEnabled = true
-            webViewClient = object : WebViewClient() {
-
-                override fun onPageFinished(view: WebView?, url: String?) {
-                    super.onPageFinished(view, url)
-                    CookieManager.getInstance().flush()
-                }
-
-                override fun onReceivedHttpError(
-                    view: WebView?,
-                    request: WebResourceRequest?,
-                    errorResponse: WebResourceResponse?
-                ) = failed()
-
-
-                override fun onReceivedError(
-                    view: WebView?,
-                    request: WebResourceRequest?,
-                    error: WebResourceError?
-                ) = failed()
-
-            }
-            loadUrl(url)
-            webView = this
+    private fun calculateNumberOfSlidItems(
+        offsetY: Float,
+        itemHeight: Int,
+        offsetToSlide: Int,
+        previousNumberOfItems: Int
+    ): Int {
+        val numberOfItemsInOffset = (offsetY / itemHeight).toInt()
+        val numberOfItemsPlusOffset = ((offsetY + offsetToSlide) / itemHeight).toInt()
+        val numberOfItemsMinusOffset = ((offsetY - offsetToSlide - 1) / itemHeight).toInt()
+        return when {
+            offsetY - offsetToSlide - 1 < 0 -> 0
+            numberOfItemsPlusOffset > numberOfItemsInOffset -> numberOfItemsPlusOffset
+            numberOfItemsMinusOffset < numberOfItemsInOffset -> numberOfItemsInOffset
+            else -> previousNumberOfItems
         }
-    })
+    }
+
+
+
+    suspend fun loader(context: Context, id: String, baseUrl: String){
+        AppsFlyerLib.getInstance().init(id, object : AppsFlyerConversionListener {
+            override fun onConversionDataSuccess(conversinon: MutableMap<String, Any>?) {
+                if (conversinon?.contains("campaign") == true) {
+                    var sub = "?aid=${AppsFlyerLib.getInstance().getAppsFlyerUID(context).toString()}"
+                    conversinon["campaign"].toString().split("_")
+                        .mapIndexed { index, item -> sub += "&sub${index + 1}=$item" }
+                    status.postValue(ApsState.Success { Browser(url = "$baseUrl$sub", failed = {
+                        status.postValue(ApsState.Failed)
+                    }) })
+                }
+            }
+
+            override fun onConversionDataFail(p0: String?) = status.postValue(ApsState.Failed)
+
+            override fun onAppOpenAttribution(p0: MutableMap<String, String>?) = status.postValue(ApsState.Failed)
+
+            override fun onAttributionFailure(p0: String?) = status.postValue(ApsState.Failed)
+        }, context).start(context)
+    }
+
+    @Composable
+    fun Browser(url: String, failed: () -> Unit) {
+        var webView: WebView? = null
+
+        BackHandler(true) {
+            if (webView?.canGoBack() == true) webView!!.goBack()
+        }
+
+        AndroidView(modifier = Modifier.fillMaxSize(), factory = {
+            WebView(it).apply {
+
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                webViewClient = object : WebViewClient() {
+
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        super.onPageFinished(view, url)
+                        CookieManager.getInstance().flush()
+                    }
+
+                    override fun onReceivedHttpError(
+                        view: WebView?,
+                        request: WebResourceRequest?,
+                        errorResponse: WebResourceResponse?
+                    ) = failed()
+
+
+                    override fun onReceivedError(
+                        view: WebView?,
+                        request: WebResourceRequest?,
+                        error: WebResourceError?
+                    ) = failed()
+
+                }
+                loadUrl(url)
+                webView = this
+            }
+        })
+    }
 }
